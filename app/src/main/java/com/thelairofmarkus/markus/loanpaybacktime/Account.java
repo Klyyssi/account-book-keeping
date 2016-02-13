e3b6c609bc9db606b891f2f0bf15e9ccc66e910b
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-02-13 23:24:45 +0200

    refactors model classes

--- app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/Account.java
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/Account.java
@@ -4,12 +4,28 @@ package com.thelairofmarkus.markus.loanpaybacktime;
  * Created by markus on 12.2.2016.
  */
 public class Account {
-    public final String accountNumber;
 
-    public Account(String accountNumber) {
+    public final String accountNumber;
+    private static final String allowedCharSeq = "[a-zA-Z]{2}[0-9]{2} ?[a-zA-Z0-9]{4} ?[0-9]{4} ?[0-9]{4} ?[0-9]{2}";
+
+    private Account(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
     @Override
     public String toString() { return accountNumber; }
+
+    public static Account fromString(String accountNumber) {
+        return accountNumber.matches(allowedCharSeq) ?
+                new Account(accountNumber) :
+                getDefault();
+    }
+
+    public static Account getDefault() {
+        return SingletonHolder.DEFAULT;
+    }
+
+    private static class SingletonHolder {
+        private static final Account DEFAULT = new Account("N/A");
+    }
 }
--- app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/Person.java
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/Person.java
@@ -4,12 +4,28 @@ package com.thelairofmarkus.markus.loanpaybacktime;
  * Created by markus on 12.2.2016.
  */
 public class Person {
-    public final String name;
 
-    public Person(String name) {
+    public final String name;
+    private static final String allowedCharSeq = "[a-zA-Z ]+";
+
+    private Person(String name) {
         this.name = name;
     }
 
     @Override
     public String toString() { return name; }
+
+    public static Person fromString(String name) {
+        return name.matches(allowedCharSeq) ?
+                new Person(name) :
+                getDefault();
+    }
+
+    public static Person getDefault() {
+        return SingletonHolder.DEFAULT;
+    }
+
+    private static class SingletonHolder {
+        private static final Person DEFAULT = new Person("N/A");
+    }
 }
