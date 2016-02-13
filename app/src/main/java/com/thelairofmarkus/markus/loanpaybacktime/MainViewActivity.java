88e50fee519e0bd0d20bd8f110f5d1bd93dbf758
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-02-13 13:18:03 +0200

    WIP ui logic

--- app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/MainViewActivity.java
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/MainViewActivity.java
@@ -1,9 +1,13 @@
 package com.thelairofmarkus.markus.loanpaybacktime;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.thelairofmarkus.markus.loanpaybacktime.fixtures.ILoanRepositoryFixImpl;
 
@@ -15,12 +19,42 @@ public class MainViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
+        updateLoans();
     }
 
-    public void newLoan(View view) {
+    public void newAccount(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
         builder.setTitle("New loan");
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        final EditText txtName = new EditText(this);
+        final EditText txtAccNumber = new EditText(this);
+
+        builder.setView(txtName);
+
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println(txtName.getText());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void updateLoans() {
+        ListView loansList = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<LoanLine> adapter = new ArrayAdapter<LoanLine>(
+                this,
+                android.R.layout.simple_list_item_1,
+                loanRepository.getLoans());
+
+        loansList.setAdapter(adapter);
     }
 }
