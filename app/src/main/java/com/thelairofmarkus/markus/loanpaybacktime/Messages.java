67f18329a6f12f12e26e4a4e5acecf789262107c
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-02-13 23:29:06 +0200

    adds class for error messages

---
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/Messages.java
@@ -0,0 +1,9 @@
+package com.thelairofmarkus.markus.loanpaybacktime;
+
+/**
+ * Created by markus on 13.2.2016.
+ */
+public class Messages {
+    public static final String ERROR_PERSON_NAME_MUST_CONTAIN_LETTERS_FROM_A_TO_Z = "Person name must contain only letters A-Z.";
+    public static final String ERROR_ACCOUNT_NUMBER_MUST_BE_FINNISH_IBAN = "Account number must be finnish IBAN.";
+}
