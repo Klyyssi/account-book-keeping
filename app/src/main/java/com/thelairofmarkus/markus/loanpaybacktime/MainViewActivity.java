bcbc28fa193ac1dd6a1d6ddb4677b7a54269a924
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-02-13 23:34:34 +0200

    adds context menu & adds validity checks for person and account

--- app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/MainViewActivity.java
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/MainViewActivity.java
@@ -4,10 +4,16 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.thelairofmarkus.markus.loanpaybacktime.fixtures.ILoanRepositoryFixImpl;
 
@@ -19,22 +25,61 @@ public class MainViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
+
+        //context menu
+        View listView = (View) findViewById(R.id.listView);
+        registerForContextMenu(listView);
+
+
         updateLoans();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.getId() == R.id.listView) {
+            menu.add("Delete");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        ListView loansList = (ListView) findViewById(R.id.listView);
+        LoanLine selectedLine = (LoanLine) loansList.getAdapter().getItem(info.position);
+        loanRepository.removeLoan(selectedLine);
+        updateLoans();
+        return true;
     }
 
     public void newAccount(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
         builder.setTitle("New loan");
-        final EditText txtName = new EditText(this);
-        final EditText txtAccNumber = new EditText(this);
 
-        builder.setView(txtName);
+        View newAccountView = getLayoutInflater().inflate(R.layout.new_account_view, null);
+        final EditText txtName = (EditText) newAccountView.findViewById(R.id.txtName);
+        final EditText txtAccNumber = (EditText) newAccountView.findViewById(R.id.txtAccNumber);
 
+        builder.setView(newAccountView);
 
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                System.out.println(txtName.getText());
+                Person person = Person.fromString(txtName.getText().toString());
+                Account account = Account.fromString(txtAccNumber.getText().toString());
+
+                if (person == Person.getDefault()) {
+                    showError(Messages.ERROR_PERSON_NAME_MUST_CONTAIN_LETTERS_FROM_A_TO_Z);
+                    return;
+                }
+
+                if (account == Account.getDefault()) {
+                    showError(Messages.ERROR_ACCOUNT_NUMBER_MUST_BE_FINNISH_IBAN);
+                    return;
+                }
+
+                LoanLine accLine = new LoanLine(person, account);
+                loanRepository.put(accLine);
+                updateLoans();
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
