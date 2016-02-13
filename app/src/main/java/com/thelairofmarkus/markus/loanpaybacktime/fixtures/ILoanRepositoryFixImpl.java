50d6573695f20a7738972efa34d3eed00f76e78f
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-02-13 23:29:58 +0200

    modifies loan repository fixture implementation

--- app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/fixtures/ILoanRepositoryFixImpl.java
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/fixtures/ILoanRepositoryFixImpl.java
@@ -5,6 +5,7 @@ import com.thelairofmarkus.markus.loanpaybacktime.ILoanRepository;
 import com.thelairofmarkus.markus.loanpaybacktime.LoanLine;
 import com.thelairofmarkus.markus.loanpaybacktime.Person;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,7 +14,12 @@ import java.util.List;
  */
 public class ILoanRepositoryFixImpl implements ILoanRepository {
 
-    List<LoanLine> loans = Arrays.asList(new LoanLine(new Person("Jaska Jokunen"), new Account("12345-6345")));
+    List<LoanLine> loans = new ArrayList<LoanLine>(
+            Arrays.asList(
+                    new LoanLine(Person.fromString("Jaska Jokunen"), Account.fromString("FI58 1234 2345 1234 58")),
+                    new LoanLine(Person.fromString("Arnold"), Account.fromString("FI12 1234 1234 1234 12"))
+            )
+    );
 
     @Override
     public void put(LoanLine loan) {
