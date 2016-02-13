1d0bdfa0c4ebd89fb716a275ed0897911bfbfe72
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-02-13 23:43:50 +0200

    fixes typo in error message

--- app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/Messages.java
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/Messages.java
@@ -5,5 +5,5 @@ package com.thelairofmarkus.markus.loanpaybacktime;
  */
 public class Messages {
     public static final String ERROR_PERSON_NAME_MUST_CONTAIN_LETTERS_FROM_A_TO_Z = "Person name must contain only letters A-Z.";
-    public static final String ERROR_ACCOUNT_NUMBER_MUST_BE_FINNISH_IBAN = "Account number must be finnish IBAN.";
+    public static final String ERROR_ACCOUNT_NUMBER_MUST_BE_FINNISH_IBAN = "Account number must be Finnish IBAN.";
 }
