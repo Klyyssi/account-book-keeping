4bf4bffa6e43c72e4aa2cdd1fe925c23ea6686d7
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-02-14 11:50:05 +0200

    documents ILoanRepository interface

--- app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/ILoanRepository.java
+++ app/src/main/java/com/thelairofmarkus/markus/loanpaybacktime/ILoanRepository.java
@@ -7,9 +7,21 @@ import java.util.List;
  */
 public interface ILoanRepository {
 
+    /**
+     * Adds new AccountLine to the repository
+     * @param loan account to add
+     */
     void put(LoanLine loan);
 
+    /**
+     * Returns all AccountLines that this repository contains
+     * @return list of AccountLines
+     */
     List<LoanLine> getLoans();
 
+    /**
+     * Removes an AccountLine from the repository
+     * @param loan AccountLine to remove
+     */
     void removeLoan(LoanLine loan);
 }
